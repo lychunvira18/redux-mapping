627d8009b05f1cce1106dde4117989129deeb234
Ly Chunvira <lychunvira18@kit.edu.kh>
2019-07-17 15:24:59 +0700

    changed from paper to cards for each posts

--- src/Components/Posts.js
+++ src/Components/Posts.js
@@ -4,7 +4,13 @@ import Grid from "@material-ui/core/Grid";
 
 import { connect } from "react-redux";
 import { fetchPosts } from "../Actions/postActions";
-import { Typography } from "@material-ui/core";
+import {
+  Typography,
+  Container,
+  Card,
+  CardMedia,
+  CardContent
+} from "@material-ui/core";
 
 class Posts extends Component {
   componentWillMount() {
@@ -14,26 +20,28 @@ class Posts extends Component {
   render() {
     const postItems = this.props.posts.map(post => {
       return (
-        <Grid item xs={3} key={post.id}>
-          <Paper
-            style={{
-              padding: 20,
-              textAlign: "center"
-            }}
-          >
-            <Typography variant="h6">{post.name}</Typography>
-            <Typography variant="body1">{post.username}</Typography>
-            <Typography variant="subtitle1">{post.email}</Typography>
-          </Paper>
+        <Grid item xs={12} sm={6} md={3} key={post.id}>
+          <Card>
+            <CardMedia
+              style={{ height: 400 }}
+              image="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909__340.png"
+              title="avatar"
+            />
+            <CardContent>
+              <Typography variant="h6">{post.name}</Typography>
+              <Typography variant="body1">{post.username}</Typography>
+              <Typography variant="subtitle1">{post.email}</Typography>
+            </CardContent>
+          </Card>
         </Grid>
       );
     });
     return (
-      <div>
+      <Container maxWidth="xl" style={{ marginTop: 30, marginBotton: 30 }}>
         <Grid style={{ flexGrow: 1 }} container spacing={3}>
           {postItems}
         </Grid>
-      </div>
+      </Container>
     );
   }
 }
